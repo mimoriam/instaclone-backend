604fbc730a13bfcaab4ac113086356fae6191049
Mimo <mimoriam420@gmail.com>
2024-11-29 02:21:28 +0500

    build(app): dotenv, cookieparser & sane defaults

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import * as Joi from 'joi';
+
+const envSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('development', 'production', 'test').required(),
+  PORT: Joi.number().port().default(3000).required(),
+});
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local'],
+      validationSchema: envSchema,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+    }),
+  ],
   controllers: [],
   providers: [],
+  exports: [ConfigModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  INestApplication,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app: INestApplication = await NestFactory.create(AppModule, {
+    cors: {
+      // * TODO: Add app/frontend URL to cors:
+      origin: ['http://localhost:3000'],
+      credentials: true,
+    },
+  });
+
+  // ? Refers to api/v{n} where n is defined in controllers
+  app.setGlobalPrefix('/api/');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // forbidNonWhitelisted: true,
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.use(cookieParser());
+
+  await app.listen(configService.get('PORT') || 3000);
 }
 
 bootstrap().then();
